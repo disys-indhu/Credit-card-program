14efa99a2af79a4a162d451eb2c6641bcb41a605
disys-indhu <95426058+disys-indhu@users.noreply.github.com>
2021-12-19 05:43:57 -0800

    Add files via upload

---
+++ Credit card program.py
@@ -0,0 +1,14 @@
+class creditcard:
+    creditlimit = 9000
+    def salary(self):
+        self.basepay = 10000
+        self.exp = "09/2023"
+    def receivecard(self):
+        print(self.basepay, self.exp)
+indhumathi=creditcard()
+indhumathi.salary()
+indhumathi.receivecard()
+
+
+
+
